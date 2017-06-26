b481ec0c200c03ce4936b06754582d21742ebdd9
takashiAg <crawd4274@gmail.com>
2017-06-26 09:23:30 +0000

    send values except unnecessary values

--- ser.py
+++ ser.py
@@ -5,11 +5,16 @@ import threading
 url="http://192.168.179.2/post.php"
 
 def  post(arg):
-    #print(arg)
-    params={'uart':arg}
-    data=urllib.urlencode(params)
-    req=urllib2.Request(url,data)
-    urllib2.urlopen(req)
+    try:
+        uart=arg.split(";")[9]
+        params={'uart':arg}
+        data=urllib.urlencode(params)
+        req=urllib2.Request(url,data)
+        urllib2.urlopen(req)
+    except IndexError:
+        print("too short")
+    except:
+        print("error")
 
 port = serial.Serial("/dev/ttyAMA0", 115200)
 
