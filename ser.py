178cd947b1c9801c2b8d6035cee6e2cc9147a835
takashiAg <crawd4274@gmail.com>
2017-06-27 16:49:16 +0900

    send only necessary messages

--- ser.py
+++ ser.py
@@ -1,22 +1,51 @@
 import serial
 import urllib,urllib2
 import threading
+import sys
 
+#post suru url
 url="http://192.168.179.2/post.php"
+
+#kono gateway de sousinn suru id wo syutokusuru url
+url_login="http://192.168.179.2/sensor.php"
+username="ando"
+password="ando"
+
+serport="/dev/ttyAMA0"
+
+ids=[]
 
 def  post(arg):
     try:
-        uart=arg.split(";")[9]
+        message=arg.split(";")
+        error_checker=message[9]
         params={'uart':arg}
-        data=urllib.urlencode(params)
-        req=urllib2.Request(url,data)
-        urllib2.urlopen(req)
+        for id in ids:
+            if id==message[5]:
+                try:
+                    data=urllib.urlencode(params)
+                    req=urllib2.Request(url,data)
+                    urllib2.urlopen(req)
+                    print("I send a message to "+url+"\nmessage:"+arg)
+                except:
+                    print("I try to send a message but missed it")
     except IndexError:
         print("too short")
     except:
         print("error")
 
-port = serial.Serial("/dev/ttyAMA0", 115200)
+#main program
+try:
+    data=urllib.urlencode({'user':username,'pass':password})
+    req=urllib2.Request(url_login,data)
+    res=urllib2.urlopen(req).read()
+    ids=res.split(",")
+except:
+    print("sensor id no syutoku ni sippai")
+    sys.exit()
+
+#sensor
+port = serial.Serial(serport, 115200)
 
 while True:
     rcv = port.readline()
